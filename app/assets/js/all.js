161813753399efbb5b70b80e9da29bea50573ff6
annanaha <bintong36@gmail.com>
2021-08-27 02:20:16 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,29 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  grid: {
+    rows: 1,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   grid: {
+//     rows: 2,
+//   },
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
