80410cd2307b207f75041ca03f0ac21ad1be58d9
annanaha <bintong36@gmail.com>
2021-08-30 21:40:52 +0800

    Update 2021-08-30T13:40:43.980Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -13,15 +13,27 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true
   }
-}); // var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   grid: {
-//     rows: 2,
-//   },
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
+}); //聽聽他們怎麼說
+
+var customerswiper = new Swiper(".customerswiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2
+  },
+  spaceBetween: 30
+}); // 課程階級選擇
+
+var courselevelswiper = new Swiper(".courselevelswiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.1
+    },
+    992: {
+      slidesPerView: 3
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
