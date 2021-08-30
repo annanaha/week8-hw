19ed6e3a2869331e1c1cf3284e57fa01b6d4e187
annanaha <bintong36@gmail.com>
2021-08-30 21:38:01 +0800

    '新增預約頁面'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,6 +3,7 @@ $(function() {
 });
 
 
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1.5,
   grid: {
@@ -16,14 +17,29 @@ var swiper = new Swiper(".mySwiper", {
   
 });
 
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   grid: {
-//     rows: 2,
-//   },
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
+//聽聽他們怎麼說
+var customerswiper = new Swiper(".customerswiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+
+});
+
+// 課程階級選擇
+var courselevelswiper = new Swiper(".courselevelswiper", {
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.1,
+    },
+    992: {
+      slidesPerView: 3,
+     
+    }
+  }
+
+});
