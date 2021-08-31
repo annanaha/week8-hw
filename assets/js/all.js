1a2682c866d6ebce672d0c3b0437835a28644bd8
annanaha <bintong36@gmail.com>
2021-09-01 02:35:23 +0800

    Update 2021-08-31T18:35:06.354Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,32 +8,40 @@ var swiper = new Swiper(".mySwiper", {
   grid: {
     rows: 1
   },
+  spaceBetween: 30
+}); //聽聽他們怎麼說
+
+var customerswiper = new Swiper(".customerswiper", {
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  //  slidesPerColumnFill: "row",
+  slidesPerGroup: 6,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true
   }
-}); //聽聽他們怎麼說
-
-var customerswiper = new Swiper(".customerswiper", {
-  slidesPerView: 3,
-  grid: {
-    rows: 2
-  },
-  spaceBetween: 30
 }); // 課程階級選擇
 
 var courselevelswiper = new Swiper(".courselevelswiper", {
-  slidesPerView: 1,
+  slidesPerView: 3,
+  grid: {
+    rows: 1
+  },
+  spaceBetween: 30
+}); //師資介紹
+
+var teacherswiper = new Swiper(".teacherswiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 1
+  },
+  spaceBetween: 30
+}); //課程推薦
+
+var recommendswiper = new Swiper(".recommendswiper", {
+  slidesPerView: 3,
   slidesPerColumn: 1,
-  spaceBetween: 20,
-  breakpoints: {
-    768: {
-      slidesPerView: 2.1
-    },
-    992: {
-      slidesPerView: 3
-    }
-  }
+  spaceBetween: 20
 });
 //# sourceMappingURL=all.js.map
