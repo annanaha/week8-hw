7e69eb748c81f61b0bc3892782fc403091122af3
annanaha <bintong36@gmail.com>
2021-09-01 17:02:40 +0800

    Update 2021-09-01T09:02:32.179Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,36 +12,106 @@ var swiper = new Swiper(".mySwiper", {
 }); //聽聽他們怎麼說
 
 var customerswiper = new Swiper(".customerswiper", {
-  slidesPerView: 3,
-  slidesPerColumn: 2,
-  //  slidesPerColumnFill: "row",
-  slidesPerGroup: 6,
+  slidesPerView: 1,
+  grid: {
+    rows: 3,
+    fill: 'row'
+  },
   spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
+  breakpoints: {
+    //  大於768
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      }
+    }
   }
 }); // 課程階級選擇
 
 var courselevelswiper = new Swiper(".courselevelswiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   grid: {
-    rows: 1
+    rows: 3,
+    fill: 'row'
   },
-  spaceBetween: 30
+  spaceBetween: 0,
+  breakpoints: {
+    //  大於768
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      }
+    }
+  }
 }); //師資介紹
 
 var teacherswiper = new Swiper(".teacherswiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   grid: {
-    rows: 1
+    rows: 4,
+    fill: 'row'
   },
-  spaceBetween: 30
+  spaceBetween: 70,
+  breakpoints: {
+    //  大於768
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      }
+    },
+    992: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+      grid: {
+        rows: 1,
+        fill: 'row'
+      }
+    }
+  }
 }); //課程推薦
 
 var recommendswiper = new Swiper(".recommendswiper", {
-  slidesPerView: 3,
-  slidesPerColumn: 1,
-  spaceBetween: 20
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
